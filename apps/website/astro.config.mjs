6a5c63b1d88544011121998367ec18a32a654931
Lonli-Lokli <lonli.lokli@gmail.com>
2023-05-18 22:12:52 +0100

    wip: building version

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,5 +2,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  outDir: '../../dist/apps/website',
   integrations: [tailwind()],
 });
