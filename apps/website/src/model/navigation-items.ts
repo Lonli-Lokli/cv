dd48f25e1562027b017fc2c2760736e0a0f306ca
Lonli-Lokli <lonli.lokli@gmail.com>
2023-05-22 12:29:51 +0100

    wip: layout of pages

---
+++ apps/website/src/model/navigation-items.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+    {
+      href: '/about',
+      label: 'About'
+    },
+    {
+      href: '/blog',
+      label: 'Blog'
+    },
+    {
+      href: '/contact',
+      label: 'Contact'
+    }
+  ];
